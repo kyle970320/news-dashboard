6ed1b14f20e38052b418e77429f27d180e42a1fb
minkyu_Park <kyle970320@farmzonesnc.com>
2025-10-24 14:45:33 +0900

    feat: add show logic in password

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../lib/supabase";
+import { Eye, EyeOff } from "lucide-react"; 
 import type { Session } from "@supabase/supabase-js";
 
 export default function LoginForm({
@@ -11,7 +12,7 @@ export default function LoginForm({
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-
+  const [show, setShow] = useState(false);
   async function handleLogin(e: React.FormEvent) {
     e.preventDefault();
     setLoading(true);
@@ -48,14 +49,23 @@ export default function LoginForm({
           required
           className="px-3 py-2 text-black border rounded w-full"
         />
-        <input
-          type="password"
-          value={password}
-          placeholder="비밀번호"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className="px-3 py-2 text-black border rounded w-full"
-        />
+        <div className="relative w-full">
+          <input
+            type={show ? "text" : "password"}
+            value={password}
+            placeholder="비밀번호"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="px-3 py-2 pr-10 text-black border rounded w-full focus:outline-none focus:ring focus:ring-indigo-200"
+          />
+          <button
+            type="button"
+            onClick={() => setShow(!show)}
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+          >
+            {show ? <EyeOff size={18} /> : <Eye size={18} />}
+          </button>
+        </div>
 
         {error && <p className="text-red-500 text-sm">{error}</p>}
 
